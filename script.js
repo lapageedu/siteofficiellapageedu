0de5439e56d05c19ae29507778eeca5307c655c2
lapageedu <lapageedu1@gmail.com>
2026-04-03 13:31:15 -0400

    le js est modifie

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-/* menu-toggle */
-
-	const toggle = document.getElementById("menu-toggle");
+const toggle = document.getElementById("menu-toggle");
 	const nav = document.getElementById("nav");
 
 	toggle.addEventListener("click", () => {
@@ -23,70 +21,39 @@
 	}, 100); // 1 seconde = 1000 ms
 
 
-// Jeu deviner le nombre 
+ // Jeu deviner le nombre 
 // on sélectionne le bouton
- let bouton = document.getElementById("bouton");
+  let bouton = document.getElementById("bouton"); 
 
-// on sélectionne la zone résultat
+ // on sélectionne la zone résultat
 let resultat = document.getElementById("resultat");
 let correspondance=document.getElementById("correspondance");
-// quand on clique sur le bouton
-bouton.addEventListener("click", function(){
+// quand on clique sur le bouton 
+let actions = [
+ "Miauler comme un chat pendant 15 secondes",
+ "Japper comme un petit chien pendant 10 secondes",
+ "Danser du compas sans musique pendant 30 secondes",
+ "Sauter à pas de crapaud pendant 15 secondes",
+ "Imiter la marche du canard pendant 20 secondes",
+ "Faire un don à LaPageEdu sur moncash-509 3814 6245",
+ "Faire une prière pour le monde",
+ "Citer 5 pays en 10 secondes",
+ "Danser une musique en ignorant son rythme",
+ "Dire une blague",
+ "Parler avec une voix drôle",
+ "Mimer le métier du maçon",
+ "Défiler pendant 30 secondes",
+ "Marcher comme un robot",
+ "Rire comme un fou",
+ "Prononcer la phrase suivante: Sur dix-huit huitres huit huitres frites s'enfuient sans bruit",
 
-    // générer un nombre aléatoire entre 1 et 10
-    let nombre = Math.floor(Math.random() * 10) + 1;
-    // afficher le nombre dans la page
-    resultat.textContent = nombre;
-if(nombre==1){
-	correspondance.textContent="Miauler comme un chat pendant 15 secondes";
-}
-else if(nombre==2){
-	correspondance.textContent="japper comme un petit chien pendant 10 secondes";
-}
-else if(nombre==3){
-	correspondance.textContent="Danser du compas sans de la music pendant 30 secondes";
-}
-else if(nombre==4){
-	correspondance.textContent="Sauter à pas de crapaud pendant 15 secondes";
-}
-else if(nombre==5){
-	correspondance.textContent="Imiter la marche du canard en cancanant pendant 20 secondes";
-}
-else if(nombre==6){
-	correspondance.textContent="Faire un don à LaPageEdu(Mon Cash:3814 6245)";
-}
-else if(nombre==7){
-	correspondance.textContent="Faire une prière pour le monde";
-}
-else if(nombre==8){
-	correspondance.textContent="Citer 5 pays en 10 secondes";
-}
-else if(nombre==9){
-	correspondance.textContent="Danser du compas sans de la music";
-}
-else if(nombre==10){
-	correspondance.textContent="Dire une blague";
-}
-else if(nombre==11){
-	correspondance.textContent="Parler avec une voix drôle pendant 30 secondes";
-}
-else if(nombre==12){
-	correspondance.textContent="Mimer le métier du macon entrain de crépir";
-}
-else if(nombre==13){
-	correspondance.textContent="Défiler pendant 30 secondes";
-}
-else if(nombre==14){
-	correspondance.textContent="Marcher comme un robot pendant 30 secondes";
-}
-else if(nombre==15){
-	correspondance.textContent="Donner 3 qualités que tu apprécies chez";
-}
-else if(nombre==16){
-	correspondance.textContent="Prononcer: Sur dix-huit huitres huit huitres frites senfuient sans bruit";
-}
+];	 
 
-
+	bouton.addEventListener("click", function(){ 
+// générer phrase aléatoire entre 1 et 16
+    let nombre = Math.floor(Math.random() * actions.length);
+    // afficher la phrase dans la page
+    correspondance.textContent = actions[nombre];	 
 }); 
 /* modal pour le jeu */
 let notice=document.getElementById("notice"); 
@@ -95,17 +62,17 @@ let fermeture=document.getElementById("fermeture");
 
 	notice.addEventListener("click", function(){
 	conteneurNotice.style.display="block";	
+	
 }
 );
 	fermeture.addEventListener("click", function(){
 	conteneurNotice.style.display="none";
+	
 }
 );
 
 
-
 /* fin */
-
 
 let ouvrir=document.getElementById("ouvrir");
 let modal=document.getElementById("modal");
@@ -128,26 +95,62 @@ let contenuAnnonces=document.getElementById("contenuAnnonces");
 
 openAnnonces.addEventListener("click", function(){
 	contenuAnnonces.style.display="block";
+openAnnonces.style.opacity="0";
 })
 
 closeAnnonces.addEventListener("click", function(){
 	contenuAnnonces.style.display="none";
+	openAnnonces.style.opacity="1";
 })
 
+/* light-box */
+
+let openbox=document.getElementById("maPhoto");
+let closeb=document.getElementById("closebox");
+let box=document.getElementById("light-box");	
+let grandePhoto=document.getElementById("grande");
+
+openbox.addEventListener("click", function(){
+	box.style.display="flex";
+	grandePhoto.src=this.src;
+})	
+
+closeb.addEventListener("click", function(e){
+	box.style.display="none";
+	if(e.target==grandePhoto){
+		box.style.display="none";
+	}
+})	
+	
 /* ACHCO */
+
+
+
+/* Cookies */
+function accepterCookies(){
+	document.getElementById("cookie-banner").style.display="none";
+} 
 
 const btn=document.getElementById("btn-1");
 const achco=document.getElementById("achco");
 const closeachco=document.getElementById("closeAchco");
 
-btn.addEventListener("click",function(){
+btn.addEventListener("click", function(){
 	achco.style.display="block";
 })
-closeachco.addEventListener("click",function(){
+closeachco.addEventListener("click", function(){
 	achco.style.display="none";
 })
-
-
-
+ /* Message affichant Bientot */
+ let ouvrirBientot=document.getElementById("openBientot");
+ let contenuBientot=document.getElementById("contenuBientot");
+ let quitteBientot=document.getElementById("quitte");
+ 
+ ouvrirBientot.addEventListener("click", function(){
+	 contenuBientot.style.display="block";
+ })
+ quitteBientot.addEventListener("click", function(){
+	 contenuBientot.style.display="none";
+ })
 
  
