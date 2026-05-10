18c2fc027b013cba0433858b54799bf2fa6bbff8
lapageedu <lapageedu1@gmail.com>
2026-05-10 00:06:15 -0400

    o ajout dans quiz.js

--- quiz.js
+++ quiz.js
@@ -1,3 +1,9 @@
+const toggle = document.getElementById("menu-toggle");
+	const nav = document.getElementById("nav");
+
+	toggle.addEventListener("click", () => {
+	nav.classList.toggle("active");
+	});
 
 		let boite=[
  {
